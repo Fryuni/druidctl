4a4b9609e9a6d0e1af0d28df83e8d6861ceda844
Luiz Ferraz <luiz@lferraz.com>
2024-07-18 02:37:46 -0300

    Update tests for `supervisor list`

--- test/commands/supervisor/list.test.ts
+++ test/commands/supervisor/list.test.ts
@@ -1,14 +1,42 @@
-import { runCommand } from '@oclif/test';
 import { expect } from 'chai';
+import nock from 'nock';
+import { succeedCommand } from '../../helper.js';
 
 describe('supervisor/list', () => {
 	it('runs supervisor/list cmd', async () => {
-		const { stdout } = await runCommand('supervisor/list');
-		expect(stdout).to.contain('hello world');
-	});
+		nock('http://localhost:8081')
+			.post('/druid/v2/sql', {
+				query: `
+SELECT
+  "supervisor_id" as "name",
+  "type",
+  "source",
+  "detailed_state",
+  "suspended" = 1 AS "suspended"
+FROM "sys"."supervisors"
+ORDER BY "supervisor_id"
+`,
+			})
+			.reply(200, [
+				{
+					name: 'alerts',
+					type: 'kafka',
+					source: 'alerts',
+					detailed_state: 'RUNNING',
+					suspended: false,
+				},
+			]);
 
-	it('runs supervisor/list --name oclif', async () => {
-		const { stdout } = await runCommand('supervisor/list --name oclif');
-		expect(stdout).to.contain('hello oclif');
+		const { result } = await succeedCommand(['supervisor', 'list']);
+
+		expect(result).to.eql([
+			{
+				name: 'alerts',
+				type: 'kafka',
+				source: 'alerts',
+				detailed_state: 'RUNNING',
+				suspended: false,
+			},
+		]);
 	});
 });
